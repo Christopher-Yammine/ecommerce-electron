b90779398fad2ab538920c3445a40159616fe3c5
ChristopherYammine <chrisyammine98@gmail.com>
2022-06-12 21:35:00 +0300

    created an electron dir and the login page tested in it

---
+++ ecom-app/Scripts/login.js
@@ -0,0 +1,35 @@
+const { default: axios } = require("axios");
+
+
+let signin=document.getElementById("signin");
+signin.addEventListener('click',login);
+
+
+function login(){
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+
+    let data =new FormData();
+    data.append("email",email);
+    data.append("password",password);
+
+    axios({
+        method:"post",
+        url:"http://127.0.0.1:8000/api/login",
+        data:data
+    }).then(function(response){
+        if(response.data.user["id_usertype"]){
+            window.location.href='../Html/adminpanel.html';
+        } else {
+            
+            
+        }
+    }).catch(function(err){
+        document.getElementById('msg').innerText="you are not allowed here/username/password incorrect"
+        setTimeout(() => {
+            document.getElementById('msg').innerText=""
+        }, 2000);
+        
+    })
+
+}
